aa5992bc72fc790d36069e2da2848eb3915ae153
Duy Tran <xtntduyx@yahoo.com>
2016-04-24 00:25:28 +0700

    006-UIforStock

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'my_portfolio' => 'users#my_portfolio'
+  get 'search_stocks' => 'stocks#search'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
